0a47dd97f98a89d03044c3b7a53073f56223549c
Pepe Segura <ppsaura@gmail.com>
2026-09-24 23:23:00 +0200

    Update para_multi_functions.php

--- para_multi_functions.php
+++ para_multi_functions.php
@@ -1,6 +1,6 @@
-<?php echo esc_url( home_url() ); ?>
 
 
-		$random_text = "echo esc_url( home_url() )" . "/?redirect_to=random" . $random_type . $tag_id;
+
+		$random_text =  esc_url( home_url() ) . "/?redirect_to=random" . $random_type . $tag_id;
 		$random_type = "<a href='" . $random_text ."'>[@]</a>";
 		return $random_type;
